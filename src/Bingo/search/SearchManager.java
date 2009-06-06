cb626da148be65fb56c59fa8cc2ffe04a56397e3
zhangjinxue <zhangjinxue@18ab2cb8-51a8-11de-971b-8d1939f35316>
2009-06-06 02:29:28 +0000

    Re-write the search code

--- src/Bingo/search/SearchManager.java
+++ src/Bingo/search/SearchManager.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 
 import Bingo.index.IndexManager;
 
@@ -60,13 +63,15 @@ public class SearchManager {
         }
         if(null != query && null != indexSearcher){			
             try {
-                Hits hits = indexSearcher.search(query);
-                for(int i = 0; i < hits.length(); i ++){
+                ScoreDoc[] hits = indexSearcher.search(query,null,100).scoreDocs;
+                for(int i = 0; i < hits.length; i ++){
                     SearchResultBean resultBean = new SearchResultBean();
-                    resultBean.setUrl(hits.doc(i).get("path"));
-                    resultBean.setTitle(hits.doc(i).get("title"));
+                    Document hitDoc = indexSearcher.doc(hits[i].doc);
+                    resultBean.setUrl(hitDoc.get("path"));
+                    resultBean.setTitle(hitDoc.get("title"));
                     searchResult.add(resultBean);
                 }
+                indexSearcher.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
